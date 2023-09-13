9b2c4f2a43401d74dabdc741e97232a30eb5bedf
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-14 05:05:58 +0530

    Creating user defined AnswerButton class for reusability

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,6 +12,41 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Question screen page');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Here this is my question',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+            ),
+          const SizedBox(height: 10),
+          AnswerButton(
+            answerText: 'Answer 1', 
+            onTap: () {
+              
+            },
+          ),
+          const SizedBox(height: 10),
+          AnswerButton(
+            answerText: 'Answer 2', 
+            onTap: () {
+              
+            },
+          ),
+          const SizedBox(height: 10),
+          AnswerButton(
+            answerText: 'Answer 3', 
+            onTap: () {
+              
+            },
+          ),
+
+        ],
+      ),
+    );
   }
 }
