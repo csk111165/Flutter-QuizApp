359db026bd66b6fbdd31df58359d28cfd51f2d6f
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-13 23:35:42 +0530

    Adding button to the StartScreen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -7,9 +7,28 @@ class StartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Center(
       child: Column(
+        // it will allow to take the min size, ie, size of the image itself
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 200,),
-
+          Image.asset('assets/images/quiz-logo.png', width: 300,),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 227, 207, 231),
+              fontSize: 20,
+            ),
+            ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {}, 
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text(
+              'Start Quiz',
+              ),
+            ),
         ],
       ),
     );
