0a5550c9a2868dce511da3d365085b2e79794ff7
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-13 23:23:16 +0530

    Loading the images

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,8 +5,13 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Start screen'),
+    return  Center(
+      child: Column(
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 200,),
+
+        ],
+      ),
     );
   }
 }
