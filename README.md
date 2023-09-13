aeda27906431b3494c3962a4dc793d0758e97329
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-13 23:16:29 +0530

    Adding gradient background color

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+
+## Notes
+* Wrapping the Text with Center will allow the background color to take up full inside the container as
+  by default Text will occupy the text area and background color be limited to that, that's the reason Text need to be wrapped with Center widget.
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: ,
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 36, 9, 109),
+                Color.fromARGB(145, 63, 35, 137)
+              ],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
     ),
-  ),);
+  );
 }
