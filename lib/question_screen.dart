f5cab0e3b196bcd0af5a2dce4cf74e8fd5632129
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-14 22:34:13 +0530

    Fixing the margin and issue (extra Container removal)

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -31,27 +31,24 @@ class _QuestionScreenState extends State<QuestionScreen> {
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(30),
-        child: Container(
-          margin: EdgeInsets.all(30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-               Text(
-                currentQuestion.text,
-                style: const TextStyle(
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-                ),
-              const SizedBox(height: 30),
-              // dynamically creating the AnswerButton using map and now adding the shuffle behaviour to the list
-              ...currentQuestion.getShuffledAnswers().map((answer) {
-                return AnswerButton(answerText: answer, onTap: answerQuestion);
-              })
-              // ... dot is required because it will allow to merget the list into column list, otherwise Column will complain as it will encounter a new separate list, ... will help to merget the all the button list with the widget list as one 
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+             Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+              ),
+            const SizedBox(height: 30),
+            // dynamically creating the AnswerButton using map and now adding the shuffle behaviour to the list
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: answerQuestion);
+            })
+            // ... dot is required because it will allow to merget the list into column list, otherwise Column will complain as it will encounter a new separate list, ... will help to merget the all the button list with the widget list as one 
+          ],
         ),
       ),
     );
