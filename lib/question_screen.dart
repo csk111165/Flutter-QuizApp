7f0bb68e52701eeca7f2d420bd98550be3c36a58
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-14 08:53:01 +0530

    Fixing margin and alignment

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -21,23 +21,27 @@ class _QuestionScreenState extends State<QuestionScreen> {
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-             Text(
-              currentQuestion.text,
-              style: const TextStyle(
-                color: Colors.white,
-              ),
-              ),
-            const SizedBox(height: 30),
-            // dynamically creating the AnswerButton using map
-            ...currentQuestion.answers.map((answer) {
-              return AnswerButton(answerText: answer, onTap: () {});
-            })
-            // ... dot is required because it will allow to merget the list into column list, otherwise Column will complain as it will encounter a new separate list, ... will help to merget the all the button list with the widget list as one 
-          ],
+        child: Container(
+          margin: EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+               Text(
+                currentQuestion.text,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+                ),
+              const SizedBox(height: 30),
+              // dynamically creating the AnswerButton using map
+              ...currentQuestion.answers.map((answer) {
+                return AnswerButton(answerText: answer, onTap: () {});
+              })
+              // ... dot is required because it will allow to merget the list into column list, otherwise Column will complain as it will encounter a new separate list, ... will help to merget the all the button list with the widget list as one 
+            ],
+          ),
         ),
       ),
     );
