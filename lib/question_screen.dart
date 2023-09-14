85c5611e271c714efc433357c1368d426acdb046
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-14 22:26:23 +0530

    Adding capability to move to the next question

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -12,10 +12,20 @@ class QuestionScreen extends StatefulWidget {
 
 class _QuestionScreenState extends State<QuestionScreen> {
 
+  var currentQuestionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
 
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
+
+    void answerQuestion() {
+
+      setState(() {
+        currentQuestionIndex++;
+      });
+      
+    }
 
     return SizedBox(
       width: double.infinity,
@@ -37,7 +47,7 @@ class _QuestionScreenState extends State<QuestionScreen> {
               const SizedBox(height: 30),
               // dynamically creating the AnswerButton using map and now adding the shuffle behaviour to the list
               ...currentQuestion.getShuffledAnswers().map((answer) {
-                return AnswerButton(answerText: answer, onTap: () {});
+                return AnswerButton(answerText: answer, onTap: answerQuestion);
               })
               // ... dot is required because it will allow to merget the list into column list, otherwise Column will complain as it will encounter a new separate list, ... will help to merget the all the button list with the widget list as one 
             ],
